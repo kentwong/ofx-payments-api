5ac6a7cff25067aa8fce49022e2c7c857eee0b08
Kent Wong <kent_wong@outlook.com>
2024-07-30 00:04:50 +1000

    feat: add currency filter to listPayments

--- src/listPayments.ts
+++ src/listPayments.ts
@@ -1,8 +1,15 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import { buildResponse } from './lib/apigateway';
-import { listPayments } from './lib/payments';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { buildResponse } from "./lib/apigateway";
+import { listPayments } from "./lib/payments";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const currency = event.queryStringParameters?.currency;
+
     const payments = await listPayments();
-    return buildResponse(200, { data: payments });
+
+    let filteredPayments = payments;
+    if (currency) {
+        filteredPayments = payments.filter((payment) => payment.currency === currency);
+    }
+    return buildResponse(200, { data: filteredPayments });
 };
