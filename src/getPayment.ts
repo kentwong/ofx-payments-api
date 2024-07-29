4389463dd90605858be283f4ac7092779f5a06ae
Kent Wong <kent_wong@outlook.com>
2024-07-29 23:03:08 +1000

    feat: implement getPayment call

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,5 +1,27 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { buildResponse } from "./lib/apigateway";
+import { getPayment } from "./lib/payments";
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    const paymentId = event.pathParameters?.id;
+
+    if (!paymentId) {
+        return buildResponse(400, {
+            error: "No payment ID provided.",
+        });
+    }
+
+    try {
+        const payment = await getPayment(paymentId);
+        if (!payment) {
+            return buildResponse(404, {
+                error: "Payment not found.",
+            });
+        }
+        return buildResponse(200, payment);
+    } catch (error) {
+        return buildResponse(500, {
+            error: "An error occurred while retrieving the payment.",
+        });
+    }
 };
