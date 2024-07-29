c8599ac6eeb3867d2e25841cce1a322007d722fd
Kent Wong <kent_wong@outlook.com>
2024-07-29 23:12:36 +1000

    feat: generate unique payment id for createPayments

--- src/createPayment.ts
+++ src/createPayment.ts
@@ -1,9 +1,11 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { buildResponse, parseInput } from './lib/apigateway';
 import { createPayment, Payment } from './lib/payments';
+import { randomUUID } from 'crypto';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const payment = parseInput(event.body || '{}') as Payment;
+    payment.id = randomUUID();
     await createPayment(payment);
     return buildResponse(201, { result: payment.id });
 };
