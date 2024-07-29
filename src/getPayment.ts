e2cc685220ca936407841fe975d25742ccca0104
Kent Wong <kent_wong@outlook.com>
2024-07-29 23:15:15 +1000

    feat: handle payment not found for getPaymets

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -7,16 +7,21 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
     if (!paymentId) {
         return buildResponse(400, {
-            error: 'No payment ID provided.', 
+            error: "No payment ID provided.",
         });
     }
-    
+
     try {
         const payment = await getPayment(paymentId);
-        return buildResponse(200, payment as Payment);
+        if (!payment) {
+            return buildResponse(404, {
+                error: "Payment not found.",
+            });
+        }
+        return buildResponse(200, payment);
     } catch (error) {
         return buildResponse(500, {
-            error: 'An error occurred while retrieving the payment.',
+            error: "An error occurred while retrieving the payment.",
         });
     }
 };
